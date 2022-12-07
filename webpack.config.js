9f09f7e5a8709a3015dd1d6bd515273002e08918
wish0ne <wish@khu.ac.kr>
2022-12-08 01:18:05 +0900

    TerserPlugin setting optimization 옵션에 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -7,6 +8,13 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].min.js",
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false, //주석을 별도의 파일로 분리할지 여부
+      }),
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
