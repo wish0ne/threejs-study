500875e8db134909323ebda34588dce1d9d4dd7d
wish0ne <wish@khu.ac.kr>
2022-12-08 01:09:11 +0900

    HtmlWebpackPlugin setting webpack.config.js파일에서 plugin옵션에 추가 src/index.html을 템플릿으로 설정 dist/index.html처럼 번들파일을 포함하는 html 파일 자동 생성됨

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -7,4 +8,10 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].min.js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      //index.html 템플릿 기반으로 빌드 결과물 추가해줌
+      template: "./src/index.html",
+    }),
+  ],
 };
