47e50bb8e3a873263a2aba15a535a11e5da57854
wish0ne <wish@khu.ac.kr>
2022-12-08 16:31:19 +0900

    render 생성 1. body에 직접 삽입하는 방법 : 새 renderer 생성해서 크기 지정 후 body에 직접 삽입 2. <canvas> 태그 미리 생성 후 거기에 삽입하는 방법 : render 생성시 canvas 옵션으로 <canvas> element에 renderer 조립

--- src/main.js
+++ src/main.js
@@ -1,3 +1,13 @@
 //entry 파일
 import * as THREE from "three";
-console.log(THREE);
+
+//동적으로 캔버스 조립하기
+// const renderer = new THREE.WebGLRenderer(); //renderer 생성
+// renderer.setSize(window.innerWidth, window.innerHeight); //renderer 크기 지정
+// document.body.appendChild(renderer.domElement); //renderer canvas 삽입 1) body에 직접 삽입
+
+//2) canvas 태그로 자리 잡아놓고 id로 삽입(추천)
+const canvas = document.querySelector("#three-canvas");
+//canvas 옵션 : renderer가 그려질 곳. dom element 지정. 없다면 새 canvas element가 생성됨
+const renderer = new THREE.WebGLRenderer({ canvas: canvas }); //canvas를 만들어놓은 canvas태그로 지정
+renderer.setSize(window.innerWidth, window.innerHeight); //renderer 크기 지정
