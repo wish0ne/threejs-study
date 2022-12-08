a3316e7e7e0604527ebeb7dece9139c1ac7a3d79
wish0ne <wish@khu.ac.kr>
2022-12-08 16:44:47 +0900

    scene, camera 생성 원하는 camera 종류에 맞게 생성 camera 시야각, 종횡비, near, far 설정 camear 위치 설정 (position) scene에 생성한 camera 추가

--- src/main.js
+++ src/main.js
@@ -11,3 +11,31 @@ const canvas = document.querySelector("#three-canvas");
 //canvas 옵션 : renderer가 그려질 곳. dom element 지정. 없다면 새 canvas element가 생성됨
 const renderer = new THREE.WebGLRenderer({ canvas: canvas }); //canvas를 만들어놓은 canvas태그로 지정
 renderer.setSize(window.innerWidth, window.innerHeight); //renderer 크기 지정
+
+//scene 생성
+const scene = new THREE.Scene();
+
+//camera 생성
+//주로 perspective camera / orthographic camera 사용
+
+//perspective camera : 원근 투영 사용. 사람의 눈으로 보는 방식을 모방하여 설계된 것으로 멀리있는건 작게, 가까이 있는건 크게 보임
+//PerspectiveCamera( fov : Number, aspect : Number, near : Number, far : Number )
+//fov — 카메라 절두체 수직 시야. (시야각)
+//aspect — 카메라 절두체 종횡비. (가로 세로 비율)
+//near — 카메라 절두체 근평면.
+//far — 카메라 절두체 원평면.
+
+//perspective camera 생성
+const camera = new THREE.PerspectiveCamera(
+  75, //시야각
+  window.innerWidth / window.innerHeight, //종횡비
+  0.1, //near
+  1000 //far
+);
+
+//camera의 default 위치는 (0,0,0)
+//camera 위치 설정 (약간 뒤로 빼줘야 물체가 잘 보임)
+camera.position.z = 5; //단위는 물체에 따라서 생각하면 편함
+
+//scene에 camera 추가
+scene.add(camera);
