22f435aed37d58f63dc4b754bd5f430760a7c55d
wish0ne <wish@khu.ac.kr>
2022-12-10 16:20:34 +0900

    fog 추가 자연스러운 원근감 추가 가능

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // import adjustBrowserSize from "./adjustBrowserSize";
 // import backgroundColor from "./backgroundColor";
 //import light from "./light";
-import animation from "./animation";
+//import animation from "./animation";
+import fog from "./fog";
 
-animation();
+fog();
