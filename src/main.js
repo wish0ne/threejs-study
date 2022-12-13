b408536707b6be8e69e02b0947da19c1ad94b95e
wish0ne <wish@khu.ac.kr>
2022-12-13 16:05:03 +0900

    geometry vertex position 조정

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import * as THREE from "three";
 import { DoubleSide } from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-//Geometry 기본
+//Geometry 정점(vertex) position 이용하기
 export default function main() {
   const canvas = document.querySelector("#three-canvas");
   const renderer = new THREE.WebGLRenderer({
@@ -25,7 +25,7 @@ export default function main() {
 
   //camera 위치 설정
   camera.position.y = 1;
-  camera.position.z = 5;
+  camera.position.z = 10;
 
   //scene에 camera 추가
   scene.add(camera);
@@ -43,15 +43,31 @@ export default function main() {
   const controls = new OrbitControls(camera, renderer.domElement);
 
   //mesh(geometry + material) 생성
-  const geometry = new THREE.BoxGeometry(1, 1, 1, 16, 16, 16); //segment : mesh가 여러도형으로 쪼개짐
-  //segement를 여러개로 쪼갤수록 vertex가 추가되는것 -> vertex들을 조절하면 geometry 자체가 바뀜
+  const geometry = new THREE.SphereGeometry(5, 64, 64);
   const material = new THREE.MeshStandardMaterial({
-    color: "seagreen",
-    side: DoubleSide, //threejs 기본은 안쪽은 안보임. 설정하면 mesh 안쪽에 들어가도 볼수있음
-    wireframe: true,
+    color: "orangered",
+    side: DoubleSide,
+    //wireframe: true,
+    flatShading: true,
   });
   const mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
+
+  //geometry.attributes.position : vertex들의 위치 담고있는 typed array(Float32) -> 특정 형식만 들어갈 수 있는 대신에 성능 빠름
+  const positionArray = geometry.attributes.position.array;
+  const randomArray = [];
+  for (let i = 0; i < positionArray.length; i += 3) {
+    //정점(vertex) 1개의 x,y,z 좌표를 랜덤으로 조정
+    positionArray[i] += (Math.random() - 0.5) * 0.2;
+    positionArray[i + 1] += (Math.random() - 0.5) * 0.2;
+    positionArray[i + 2] += (Math.random() - 0.5) * 0.2;
+
+    randomArray.push(
+      (Math.random() - 0.5) * 0.2,
+      (Math.random() - 0.5) * 0.2,
+      (Math.random() - 0.5) * 0.2
+    );
+  }
 
   //AxesHelper
   const axesHelper = new THREE.AxesHelper(5);
@@ -61,6 +77,16 @@ export default function main() {
   const clock = new THREE.Clock();
   function draw() {
     const delta = clock.getDelta();
+    const time = clock.getElapsedTime() * 5;
+
+    for (let i = 0; i < positionArray.length; i += 3) {
+      //정점(vertex) 1개의 x,y,z 좌표를 랜덤으로 조정
+      positionArray[i] += Math.sin(randomArray[i] * 100 + time) * 0.003;
+      positionArray[i + 1] += Math.sin(randomArray[i + 1] * 100 + time) * 0.003;
+      positionArray[i + 2] += Math.sin(randomArray[i + 2] * 100 + time) * 0.003;
+    }
+
+    geometry.attributes.position.needsUpdate = true;
 
     renderer.render(scene, camera);
     renderer.setAnimationLoop(draw);
