ba8e841bb13358e429fb9986c93593ad0873c224
wish0ne <wish@khu.ac.kr>
2023-04-06 09:29:22 +0900

    여러 이미지 텍스쳐가 적용된 큐브

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
-// 텍스쳐 이미지 변환
+// 여러가지 텍스쳐가 적용된 큐브
 export default function main() {
   //텍스쳐 이미지 로드
   const loadingManager = new THREE.LoadingManager();
@@ -18,24 +18,41 @@ export default function main() {
     console.log("에러");
   };
   const textureLoader = new THREE.TextureLoader(loadingManager);
-  const texture = textureLoader.load(
-    "/textures/skull/Ground Skull_basecolor.jpg"
-  );
+  const rightTexture = textureLoader.load("/textures/mcstyle/right.png");
+  const backTexture = textureLoader.load("/textures/mcstyle/back.png");
+  const bottomTexture = textureLoader.load("/textures/mcstyle/bottom.png");
+  const frontTexture = textureLoader.load("/textures/mcstyle/front.png");
+  const topTextrue = textureLoader.load("/textures/mcstyle/top.png");
+  const leftTexture = textureLoader.load("/textures/mcstyle/left.png");
 
-  //텍스쳐 변환
-  texture.wrapS = THREE.RepeatWrapping;
-  texture.wrapT = THREE.RepeatWrapping;
+  const materials = [
+    new THREE.MeshBasicMaterial({
+      map: rightTexture,
+    }),
+    new THREE.MeshBasicMaterial({
+      map: leftTexture,
+    }),
+    new THREE.MeshBasicMaterial({
+      map: topTextrue,
+    }),
+    new THREE.MeshBasicMaterial({
+      map: bottomTexture,
+    }),
+    new THREE.MeshBasicMaterial({
+      map: frontTexture,
+    }),
+    new THREE.MeshBasicMaterial({
+      map: backTexture,
+    }),
+  ];
 
-  // texture.offset.x = 0.3;
-  // texture.offset.y = 0.3;
-
-  // texture.repeat.x = 2;
-  // texture.repeat.y = 2;
-
-  //texture.rotation = Math.PI * 0.25;
-  texture.rotation = THREE.MathUtils.degToRad(60);
-  texture.center.x = 0.5;
-  texture.center.y = 0.5;
+  //작은 픽셀 사이즈 작업할때 선명하게 보이게 하기 위함
+  rightTexture.magFilter = THREE.NearestFilter;
+  leftTexture.magFilter = THREE.NearestFilter;
+  topTextrue.magFilter = THREE.NearestFilter;
+  bottomTexture.magFilter = THREE.NearestFilter;
+  backTexture.magFilter = THREE.NearestFilter;
+  frontTexture.magFilter = THREE.NearestFilter;
 
   const canvas = document.querySelector("#three-canvas");
   const renderer = new THREE.WebGLRenderer({
@@ -77,10 +94,7 @@ export default function main() {
   //mesh(geometry + material) 생성
   const geometry = new THREE.BoxGeometry(2, 2, 2);
 
-  const material = new THREE.MeshStandardMaterial({
-    map: texture,
-  });
-  const mesh = new THREE.Mesh(geometry, material);
+  const mesh = new THREE.Mesh(geometry, materials);
   scene.add(mesh);
 
   //AxesHelper
