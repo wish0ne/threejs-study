af6082fdf4019ad43066d6fc30fa4044475ad0ed
wish0ne <wish@khu.ac.kr>
2023-04-07 09:10:02 +0900

    Light 기본 사용법

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import dat from "dat.gui";
 
+// Light 기본
 export default function main() {
   const canvas = document.querySelector("#three-canvas");
   const renderer = new THREE.WebGLRenderer({
@@ -31,22 +34,45 @@ export default function main() {
   const ambientLight = new THREE.AmbientLight("white", 0.5);
   scene.add(ambientLight);
 
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 1); //빛 색상, 강도
-  directionalLight.position.x = 1;
-  directionalLight.position.z = 2;
-  scene.add(directionalLight);
+  const light = new THREE.DirectionalLight("white", 0.5);
+  light.position.y = 3;
+  scene.add(light);
 
-  //mesh(geometry + material) 생성
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshStandardMaterial({
-    color: "seagreen",
-  });
-  const mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
+  const lightHelper = new THREE.DirectionalLightHelper(light);
+  scene.add(lightHelper);
+
+  //Controls
+  const controls = new OrbitControls(camera, renderer.domElement);
+
+  //Geometry
+  const planeGeometry = new THREE.PlaneGeometry(10, 10);
+  const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+  const sphereGeometry = new THREE.SphereGeometry(0.7, 16, 16);
+
+  //Material
+  const material1 = new THREE.MeshStandardMaterial({ color: "white" });
+  const material2 = new THREE.MeshStandardMaterial({ color: "royalblue" });
+  const material3 = new THREE.MeshStandardMaterial({ color: "gold" });
+
+  //Mesh
+  const plane = new THREE.Mesh(planeGeometry, material1);
+  const box = new THREE.Mesh(boxGeometry, material2);
+  const sphere = new THREE.Mesh(sphereGeometry, material3);
+
+  plane.rotation.x = Math.PI * -0.5;
+  box.position.set(1, 1, 0);
+  sphere.position.set(-1, 1, 0);
+  scene.add(plane, box, sphere);
 
   //AxesHelper
   const axesHelper = new THREE.AxesHelper(5);
   scene.add(axesHelper);
+
+  //Dat GUI
+  const gui = new dat.GUI();
+  gui.add(light.position, "x", -5, 5);
+  gui.add(light.position, "y", -5, 5);
+  gui.add(light.position, "z", -5, 5);
 
   //animation
   const clock = new THREE.Clock();
