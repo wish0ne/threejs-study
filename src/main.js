1466f0c360f8fb2b4046bf9e16e5cbd439945112
wish0ne <wish@khu.ac.kr>
2023-04-08 11:23:39 +0900

    드래그 클릭 방지

--- src/main.js
+++ src/main.js
@@ -76,6 +76,7 @@ export default function main() {
   }
 
   function checkIntersects() {
+    if (mouseMoved) return;
     raycaster.setFromCamera(mouse, camera); //카메라 시점에서 마우스 클릭 좌표로 광선
 
     const intersects = raycaster.intersectObjects(meshes);
@@ -104,6 +105,24 @@ export default function main() {
     mouse.y = -((e.clientY / canvas.clientHeight) * 2 - 1);
     //console.log(mouse);
     checkIntersects();
+  });
+
+  let mouseMoved;
+  let clickStartX;
+  let clickStartY;
+  let clickStartTime;
+  canvas.addEventListener("mousedown", (e) => {
+    clickStartX = e.clientX;
+    clickStartY = e.clientY;
+    clickStartTime = Date.now();
+  });
+  canvas.addEventListener("mouseup", (e) => {
+    const xGap = Math.abs(e.clientX - clickStartX);
+    const yGap = Math.abs(e.clientY - clickStartY);
+    const timeGap = Date.now() - clickStartTime;
+
+    if (xGap > 5 || yGap > 5 || timeGap > 0.5) mouseMoved = true;
+    else mouseMoved = false;
   });
 
   draw();
